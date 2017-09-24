f853307849e5bd13f0670d36994b5b700f996aba
Peter Brennan <peter.brennan@wsu.edu>
2017-09-23 21:22:22 -0700

    Rewrote MainMenu script

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+}
